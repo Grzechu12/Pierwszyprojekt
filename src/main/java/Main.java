b2d7f3123725ae1b9f852cf8bd8c2f2a3746b460
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-02-28 16:18:48 +0000

    zmiana 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Cześć mateusz kolego!");
+    System.out.println("Cześć wiktor kolego!");
   }
 
   // @Test
   // void addition() {
-  //     assertEquals(2, 1 + 1);
+  // assertEquals(2, 1 + 1);
   // }
 }
