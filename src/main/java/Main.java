88ea883411f4e28153325986bacb3aa98560926e
Grzechu12 <161622135+Grzechu12@users.noreply.github.com>
2024-02-28 16:23:21 +0000

    poprawa 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Cześć mateusz kolego!");
     System.out.println("Cześć wiktor kolego!");
+    System.out.println("szukam telefonu");
   }
 
   // @Test
